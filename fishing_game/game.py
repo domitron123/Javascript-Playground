c7fdecaebdb972fb8a1de1fb892a78af80c891a1
domitron123 <domtownshend@hotmail.com>
2023-08-14 10:46:51 +1200

    started idea for a game perhaps

---
+++ fishing_game/game.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
+from tkinter import font
+import random
+import time
+import os
+import sys
+import threading
+import json
+
+# create basic window 
+root = tk.Tk()
+
+window.title("Fishing Game")
+window.geometry("800x500")
+window.pack()
+
+
+root.mainloop()
--- src/testing.js
+++ src/testing.js
@@ -1,9 +1,71 @@
-function checkScope() {
-    var i = 'function scope';
-    if (true) {
-      i = 'block scope';
-      console.log('Block scope i is: ', i);
-    }
-    console.log('Function scope i is: ', i);
-    return i;
-  }
+// function checkScope() {
+//     var i = 'function scope';
+//     if (true) {
+//       i = 'block scope';
+//       console.log('Block scope i is: ', i);
+//     }
+//     console.log('Function scope i is: ', i);
+//     return i;
+//   }
+
+// function objectArray(arr) {
+//   let newArray = [], newObj = {};
+
+//   arr.forEach(el => {
+//     for(let i = 0; i < (el['last_name']); i++) {
+//         newObj['first_name'] = el.first_name;
+//         newObj['last_name'] = el.last_name;
+//         newArray.push(newObj)
+//         newObj = {}
+//       }
+//     })
+//     return newArray
+// }
+
+
+
+// console.log(objectArray([{first_name: "Daniel", last_name: "Smith"}, {first_name: "Michael", last_name: "Brown"}]))
+
+
+// const objectArray = arr => arr.reduce((a, b) =>{
+//   for(let i = 0; i <  blur.last_name; i++) {
+//     arr.push({...b, last_name: last_name})
+//   }
+//   return a
+// }, [])
+
+
+// NOTE - const obj = {first_name: "Dom", last_name: "Townshend"}
+
+// let {first_name: "Dom", last_name: "Townshend"} = obj
+
+// const str = `({first_name: "Dom", last_name: last = "Townshend"} = {first_name: "Dom", last_name: "Townshend"}).toString()`
+
+// console.log(first_name)
+
+// write function that will revers a string by word
+
+// console.log(reverseString("Hello World")) -> "World Hello"
+
+// function reverseWord(str) {
+//   return str.split(" ").reverse().join(" ")
+// }
+
+// console.log(reverseWord("Hello World"))
+
+// create a function that switches the key value of an object
+
+// objfunc({"name": "Dom"}) -> {"Dom": "name"}
+
+function objfunc(obj) {
+  return Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => [value, key]
+  ))
+  // let newObj = {}
+  // for(let key in obj) {
+  //   newObj[obj[key]] = key
+  // }
+  // return newObj
+}
+
+console.log(objfunc({"name": "Dom"}))
